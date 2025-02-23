55f45a334ac85c504c3f28a88a601609d7dc447a
BoyJayy <popkov.ad@icloud.com>
2025-02-23 18:31:16 +0300

    Add files via upload

---
+++ Суммы/2D matrix pref sum.cpp
@@ -0,0 +1,30 @@
+vector<vector<int>> computePrefixSum(const vector<vector<int>> &A) {
+    int n = A.size();
+    int m = A[0].size();
+    vector<vector<int>> P(n, vector<int>(m, 0));
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            P[i][j] = A[i][j];
+            if (i > 0)
+                P[i][j] += P[i - 1][j];    // Добавляем сумму сверху
+            if (j > 0)
+                P[i][j] += P[i][j - 1];    // Добавляем сумму слева
+            if (i > 0 && j > 0)
+                P[i][j] -= P[i - 1][j - 1]; // Вычитаем перекрывающуюся область
+        }
+    }
+    return P;
+}
+
+// Функция для вычисления суммы подматрицы с координатами (x1, y1) - верхний левый угол и (x2, y2) - нижний правый угол
+int submatrixSum(const vector<vector<int>> &P, int x1, int y1, int x2, int y2) {
+    int sum = P[x2][y2];
+    if (x1 > 0)
+        sum -= P[x1 - 1][y2];
+    if (y1 > 0)
+        sum -= P[x2][y1 - 1];
+    if (x1 > 0 && y1 > 0)
+        sum += P[x1 - 1][y1 - 1];
+    return sum;
+}
